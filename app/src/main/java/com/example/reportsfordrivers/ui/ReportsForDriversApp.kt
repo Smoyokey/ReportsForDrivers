8d721d46cf73b112f62914fe73ac322d36b2ede3
Smoyokey <oleg37306@gmail.com>
2024-01-24 09:25:10 +0300

    one commit

---
+++ app/src/main/java/com/example/reportsfordrivers/ui/ReportsForDriversApp.kt
@@ -0,0 +1,16 @@
+package com.example.reportsfordrivers.ui
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+//@Composable
+//fun ReportsForDriversApp(
+//    modifier: Modifier = Modifier
+//) {
+//    Scaffold(
+//        topBar = {}
+//    ) {
+//
+//    }
+//}
