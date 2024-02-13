e0f235d9cfb798d0b4f1d68d1b520827216a267c
Smoyokey <oleg37306@gmail.com>
2024-02-13 09:35:08 +0300

    Commit dataFillingTwo

--- app/src/main/java/com/example/reportsfordrivers/Tags.kt
+++ app/src/main/java/com/example/reportsfordrivers/Tags.kt
@@ -11,4 +11,10 @@ object Tags {
     const val TAG_TEST_FIRST_ENTRY_RN: String = "tagTestFirstEntryRn"
     const val TAG_TEST_FIRST_ENTRY_TABLE: String = "tagTestFirstEntryTable"
     const val TAG_TEST_FIRST_ENTRY_BUTTON_TABLE: String = "tagTestFirstEntryButtonTable"
+    const val TAG_TEST_DATA_FILLING_TWO_ROUTE: String = "tagTestDataFillingTwoRoute"
+    const val TAG_TEST_DATA_FILLING_TWO_SPEEDOMETER_DEPARTURE: String =
+        "tagTestDataFillingTwoSpeedometerDeparture"
+    const val TAG_TEST_DATA_FILLING_TWO_SPEEDOMETER_RETURN: String =
+        "tagTestDataFillingTwoSpeedometerReturn"
+    const val TAG_TEST_DATA_FILLING_TWO_FUELLED: String = "tagTestDataFillingTwoFuelled"
 }
--- app/src/main/java/com/example/reportsfordrivers/ui/layouts/createreports/CreateReportsDataFillingTwoScreen.kt
+++ app/src/main/java/com/example/reportsfordrivers/ui/layouts/createreports/CreateReportsDataFillingTwoScreen.kt
@@ -1,17 +1,22 @@
 package com.example.reportsfordrivers.ui.layouts.createreports
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -20,6 +25,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.reportsfordrivers.R
+import com.example.reportsfordrivers.Tags
 
 @Composable
 fun CreateReportsDataFillingTwoScreen(
@@ -41,17 +47,33 @@ fun CreateReportsDataFillingTwoScreen(
         Divider(
             modifier = Modifier.padding(10.dp)
         )
-
-        OutlinedTextFieldDataFillingTwo(R.string.route)
+        
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = stringResource(R.string.route)) },
+            modifier = Modifier.fillMaxWidth(),
+            trailingIcon = {
+                if(true) {
+                    Icon(
+                        imageVector = Icons.Outlined.Clear,
+                        contentDescription = stringResource(R.string.clear),
+                        modifier = Modifier
+                            .clickable {
+//                                onValueChange(itemDetails.copy(route = ""))
+                            }
+                            .testTag(Tags.TAG_TEST_DATA_FILLING_TWO_ROUTE)
+                    )
+                }
+            }
+        )
 
         LineDataFilling(R.string.date_departure)
         LineDataFilling(R.string.date_return)
         LineDataFilling(R.string.date_border_crossing_departure)
         LineDataFilling(R.string.date_border_crossing_return)
 
-        OutlinedTextFieldDataFillingTwo(R.string.speedometer_reading_departure)
-        OutlinedTextFieldDataFillingTwo(R.string.speedometer_reading_return)
-        OutlinedTextFieldDataFillingTwo(R.string.fuelled)
+        OutlinedTextFieldDataFillingTwo()
 
         Column(
             modifier = Modifier.weight(1f)
@@ -73,20 +95,6 @@ fun CreateReportsDataFillingTwoScreen(
 }
 
 @Composable
-fun OutlinedTextFieldDataFillingTwo(label: Int) {
-    OutlinedTextField(
-        value = "",
-        onValueChange = {},
-        label = {
-            Text(
-                text = stringResource(label)
-            )
-        },
-        modifier = Modifier.fillMaxWidth()
-    )
-}
-
-@Composable
 fun LineDataFilling(text: Int) {
     Row(
         modifier = Modifier.fillMaxWidth(),
@@ -103,6 +111,72 @@ fun LineDataFilling(text: Int) {
                 text = stringResource(R.string.date)
             )
         }
+    }
+}
+
+@Composable
+fun OutlinedTextFieldDataFillingTwo() {
+    Column() {
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = stringResource(R.string.speedometer_reading_departure)) },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            trailingIcon = {
+                if(true) {
+                    Icon(
+                        imageVector = Icons.Outlined.Clear,
+                        contentDescription = stringResource(R.string.clear),
+                        modifier = Modifier
+                            .clickable {
+//                                onValueChange(itemDetails.copy(tt = ""))
+                            }
+                            .testTag(Tags.TAG_TEST_DATA_FILLING_TWO_SPEEDOMETER_DEPARTURE)
+                    )
+                }
+            }
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = stringResource(R.string.speedometer_reading_return)) },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            trailingIcon = {
+                if(true) {
+                    Icon(
+                        imageVector = Icons.Outlined.Clear,
+                        contentDescription = stringResource(R.string.clear),
+                        modifier = Modifier
+                            .clickable {
+//                                onValueChange(itemDetails.copy(tt = ""))
+                            }
+                            .testTag(Tags.TAG_TEST_DATA_FILLING_TWO_SPEEDOMETER_RETURN)
+                    )
+                }
+            }
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = stringResource(R.string.fuelled)) },
+            modifier = Modifier.fillMaxWidth(),
+            singleLine = true,
+            trailingIcon = {
+                if(true) {
+                    Icon(
+                        imageVector = Icons.Outlined.Clear,
+                        contentDescription = stringResource(R.string.clear),
+                        modifier = Modifier
+                            .clickable {
+//                                onValueChange(itemDetails.copy(tt = ""))
+                            }
+                            .testTag(Tags.TAG_TEST_DATA_FILLING_TWO_FUELLED)
+                    )
+                }
+            }
+        )
     }
 }
 
