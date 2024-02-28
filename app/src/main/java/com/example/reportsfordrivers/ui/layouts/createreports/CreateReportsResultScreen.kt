2e047d63b4bf0b9aa3ee3efad857c8fffebf6b99
Smoyokey <oleg37306@gmail.com>
2024-02-28 11:34:47 +0300

    Save file done

--- app/src/main/java/com/example/reportsfordrivers/ui/layouts/createreports/CreateReportsResultScreen.kt
+++ app/src/main/java/com/example/reportsfordrivers/ui/layouts/createreports/CreateReportsResultScreen.kt
@@ -1,5 +1,10 @@
 package com.example.reportsfordrivers.ui.layouts.createreports
 
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,15 +18,29 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.reportsfordrivers.R
 import com.example.reportsfordrivers.viewmodel.createreports.CreateReportsViewModel
+
+private const val TAG = "CreateReportsResultScreen"
 
 @Composable
 fun CreateReportsResultScreen(
     viewModel: CreateReportsViewModel = hiltViewModel<CreateReportsViewModel>()
 ) {
     val context = LocalContext.current
+    val permission = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        if(it) {
+            Log.i(TAG, "Permission granted")
+            viewModel.saveFile(context)
+        } else {
+            Log.i(TAG, "Permission denied")
+        }
+    }
+
     Column(
         modifier = Modifier.padding(start = 10.dp, end = 10.dp)
     ) {
@@ -41,7 +60,16 @@ fun CreateReportsResultScreen(
 
             Button(
                 onClick = {
-                    viewModel.saveFile(context)
+                    if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                        val permissionCheckResult = ContextCompat.checkSelfPermission(context, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        if(permissionCheckResult == PackageManager.PERMISSION_GRANTED) {
+                            viewModel.saveFile(context)
+                        } else {
+                            permission.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        }
+                    } else {
+                        viewModel.saveFile(context)
+                    }
                 },
                 modifier = Modifier.fillMaxWidth()
             ) {
--- app/src/main/java/com/example/reportsfordrivers/viewmodel/createreports/CreateReportsViewModel.kt
+++ app/src/main/java/com/example/reportsfordrivers/viewmodel/createreports/CreateReportsViewModel.kt
@@ -1,11 +1,19 @@
 package com.example.reportsfordrivers.viewmodel.createreports
 
 import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.mutableStateOf
+import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModel
 import com.example.reportsfordrivers.datastore.fiofirstentry.FioFirstEntryRepository
@@ -55,6 +63,8 @@ class   CreateReportsViewModel @Inject constructor(
 
     var tabIndex = mutableStateOf(0)
     val tabs = listOf("1", "2", "3", "4", "5", "6")
+
+
 
     private fun updateDataReportInfo(dataReportInfo: DataReportInfo) {
         uiState.value = uiState.value.copy(dataReportInfo = dataReportInfo)
@@ -328,6 +338,7 @@ class   CreateReportsViewModel @Inject constructor(
 
     fun saveFile(context: Context) {
         writeFile("download/", "Print.doc", context)
+        Log.i(TAG, "File create")
     }
 
     fun testShare(context: Context) {
@@ -367,24 +378,39 @@ class   CreateReportsViewModel @Inject constructor(
     }
 
     private fun writeFile(filePath: String, fileName: String, context: Context) {
-        try {
-            //Создается объект файла, при этом путь к файлу находиться методом Environment
-            val myFile = File(
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName
-            )
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val contentValues = ContentValues().apply {
+                put(MediaStore.Downloads.DISPLAY_NAME, fileName)
+                put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
+            }
+            val resolver = context.contentResolver
+            val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
+
+            if(uri != null) {
+                val fileOutputStream = resolver.openOutputStream(uri)
+                fileOutputStream?.write(onePageHtml().toByteArray())
+                fileOutputStream?.close()
+            }
+        } else {
+            try {
+                //Создается объект файла, при этом путь к файлу находиться методом Environment
+                val myFile = File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName
+                )
 //            val myFile = File(context.filesDir, fileName)
-            // Создается файл, если он не был создан
-            myFile.delete()
-            myFile.createNewFile()
-            // После чего создаем поток для записи
-            val outputStream = FileOutputStream(myFile)
-            // Производим непосредственно запись
-            outputStream.write(onePageHtml().toByteArray())
-            // Закрываем поток
-            outputStream.close()
-            // Просто для удобства визуального контроля исполнения метода в приложении
-        } catch (e: Exception) {
-            e.printStackTrace()
+                // Создается файл, если он не был создан
+                myFile.delete()
+                myFile.createNewFile()
+                // После чего создаем поток для записи
+                val outputStream = FileOutputStream(myFile)
+                // Производим непосредственно запись
+                outputStream.write(onePageHtml().toByteArray())
+                // Закрываем поток
+                outputStream.close()
+                // Просто для удобства визуального контроля исполнения метода в приложении
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 
