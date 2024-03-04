bbb2c2a03fbf05e5077faa4cfba0c97ceb895dab
Smoyokey <oleg37306@gmail.com>
2024-03-04 19:25:52 +0300

    Version Two March 4 19:25

--- app/src/androidTest/kotlin/createuitest/PersonalInfoUiTest.kt
+++ app/src/androidTest/kotlin/createuitest/PersonalInfoUiTest.kt
@@ -1,4 +1,149 @@
 package createuitest
 
+import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTextInput
+import com.example.reportsfordrivers.MainActivity
+import com.example.reportsfordrivers.R
+import com.example.reportsfordrivers.Tags
+import com.example.reportsfordrivers.navigationtest.helpmethods.onNodeWithStringId
+import com.example.reportsfordrivers.ui.layouts.createreports.CreateReportsDataPersonalInfoScreen
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
 class PersonalInfoUiTest {
+
+    private val lastName = "Ivanov"
+    private val firstName = "Ivan"
+    private val patronymic = "Ivanovich"
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule(MainActivity::class.java)
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+        composeRule.activity.setContent {
+            CreateReportsDataPersonalInfoScreen()
+        }
+    }
+
+    @Test
+    fun writeText_inTextFieldLastName() {
+        actionWrite(R.string.last_name, lastName)
+        composeRule.onNodeWithText(lastName).assertIsDisplayed()
+    }
+
+    @Test
+    fun writeText_inTextFieldFirstName() {
+        actionWrite(R.string.first_name, firstName)
+        composeRule.onNodeWithText(firstName).assertIsDisplayed()
+    }
+
+    @Test
+    fun writeText_inTextFieldPatronymic() {
+        actionWrite(R.string.patronymic, patronymic)
+        composeRule.onNodeWithText(patronymic).assertIsDisplayed()
+    }
+
+    @Test
+    fun writeText_inTextFieldLastNameFirstNamePatronymic() {
+        actionWrite(R.string.last_name, lastName)
+        actionWrite(R.string.first_name, firstName)
+        actionWrite(R.string.patronymic, patronymic)
+        composeRule.apply {
+            onNodeWithText(lastName).assertIsDisplayed()
+            onNodeWithText(firstName).assertIsDisplayed()
+            onNodeWithText(patronymic).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun isVisible_buttonClear_onTextFieldLastName() {
+        actionWrite(R.string.last_name, lastName)
+        composeRule.onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_LAST_NAME).assertIsDisplayed()
+    }
+
+    @Test
+    fun isVisible_buttonClear_onTextFieldFirstName() {
+        actionWrite(R.string.first_name, firstName)
+        composeRule.onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_FIRST_NAME).assertIsDisplayed()
+    }
+
+    @Test
+    fun isVisible_buttonClear_onTextFieldPatronymic() {
+        actionWrite(R.string.patronymic, patronymic)
+        composeRule.onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_PATRONYMIC).assertIsDisplayed()
+    }
+
+    @Test
+    fun isVisible_buttonClear_onTextFieldLastNameFirstNamePatronymic() {
+        actionWrite(R.string.last_name, lastName)
+        actionWrite(R.string.first_name, firstName)
+        actionWrite(R.string.patronymic, patronymic)
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_LAST_NAME).assertIsDisplayed()
+            onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_FIRST_NAME).assertIsDisplayed()
+            onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_PATRONYMIC).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun clickButton_inTextFieldLastName() {
+        actionWrite(R.string.last_name, lastName)
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_LAST_NAME).performClick()
+            onNodeWithText(lastName).assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun clickButton_inTextFieldFirstName() {
+        actionWrite(R.string.first_name, firstName)
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_FIRST_NAME).performClick()
+            onNodeWithText(firstName).assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun clickButton_inTextFieldPatronymic() {
+        actionWrite(R.string.patronymic, patronymic)
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_PATRONYMIC).performClick()
+            onNodeWithText(patronymic).assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun clickButtons_inTextFieldLastNameFirstNamePatronymic() {
+        actionWrite(R.string.last_name, lastName)
+        actionWrite(R.string.first_name, firstName)
+        actionWrite(R.string.patronymic, patronymic)
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_LAST_NAME).performClick()
+            onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_FIRST_NAME).performClick()
+            onNodeWithTag(Tags.TAG_TEST_DATA_PERSONAL_INFO_PATRONYMIC).performClick()
+            onNodeWithText(lastName).assertDoesNotExist()
+            onNodeWithText(firstName).assertDoesNotExist()
+            onNodeWithText(patronymic).assertDoesNotExist()
+        }
+    }
+    private fun actionWrite(@StringRes label: Int, text: String) {
+        composeRule.onNodeWithStringId(label)
+            .performClick()
+            .performTextInput(text)
+    }
 }
--- app/src/androidTest/kotlin/createuitest/ReportInfoUiTest.kt
+++ app/src/androidTest/kotlin/createuitest/ReportInfoUiTest.kt
@@ -4,11 +4,13 @@ import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import com.example.reportsfordrivers.MainActivity
 import com.example.reportsfordrivers.R
+import com.example.reportsfordrivers.Tags
 import com.example.reportsfordrivers.navigationtest.helpmethods.onNodeWithStringId
 import com.example.reportsfordrivers.ui.layouts.createreports.CreateReportsDataReportInfoScreen
 import dagger.hilt.android.testing.HiltAndroidRule
@@ -16,11 +18,13 @@ import dagger.hilt.android.testing.HiltAndroidTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.Date
 
 @HiltAndroidTest
 class ReportInfoUiTest {
 
-    private val waybill = "1"
+    private val waybill = "123"
     private val township = "Minsk"
 
     @get:Rule(order = 0)
@@ -49,9 +53,114 @@ class ReportInfoUiTest {
         composeRule.onNodeWithText(township).assertIsDisplayed()
     }
 
+    @Test
+    fun clickDate_inDateCreateReport() {
+        composeRule.apply {
+            clickDate()
+            onNodeWithText(currentDate()).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun textFields_inWaybillCityDate() {
+        composeRule.apply {
+            actionWrite(R.string.waybill, waybill)
+            actionWrite(R.string.township, township)
+            onNodeWithText(waybill).assertIsDisplayed()
+            onNodeWithText(township).assertIsDisplayed()
+            clickDate()
+            onNodeWithText(currentDate()).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun isVisible_buttonClear_inTextFieldWaybill() {
+        actionWrite(R.string.waybill, waybill)
+        composeRule.onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_WAYBILL).assertIsDisplayed()
+    }
+
+    @Test
+    fun isVisible_buttonClear_inTextFieldCity() {
+        actionWrite(R.string.township, township)
+        composeRule.onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_MAIN_CITY).assertIsDisplayed()
+    }
+
+    @Test
+    fun isVisible_buttonClear_inTextFieldDate() {
+        clickDate()
+        composeRule.onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_DATE).assertIsDisplayed()
+    }
+
+    @Test
+    fun isVisible_buttonClear_inTextFieldWaybillCityDate() {
+        actionWrite(R.string.waybill, waybill)
+        actionWrite(R.string.township, township)
+        clickDate()
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_WAYBILL).assertIsDisplayed()
+            onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_MAIN_CITY).assertIsDisplayed()
+            onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_DATE).assertIsDisplayed()
+        }
+    }
+
+    @Test
+    fun clickButton_inTextFieldWaybill() {
+        actionWrite(R.string.waybill, waybill)
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_WAYBILL).performClick()
+            onNodeWithText(waybill).assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun clickButton_inTextFieldCity() {
+        actionWrite(R.string.township, township)
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_MAIN_CITY).performClick()
+            onNodeWithText(township).assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun clickButton_inTextFieldDate() {
+        clickDate()
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_DATE).performClick()
+            onNodeWithText(currentDate()).assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun clickButtons_inTextFieldWaybillCityDate() {
+        actionWrite(R.string.waybill, waybill)
+        actionWrite(R.string.township, township)
+        clickDate()
+        composeRule.apply {
+            onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_WAYBILL).performClick()
+            onNodeWithText(waybill).assertDoesNotExist()
+            onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_MAIN_CITY).performClick()
+            onNodeWithText(township).assertDoesNotExist()
+            onNodeWithTag(Tags.TAG_TEST_DATA_REPORT_INFO_DATE).performClick()
+            onNodeWithText(currentDate()).assertDoesNotExist()
+        }
+    }
+
     private fun actionWrite(@StringRes label: Int, text: String) {
         composeRule.onNodeWithStringId(label)
             .performClick()
             .performTextInput(text)
+    }
+
+    private fun clickDate() {
+        composeRule.apply {
+            onNodeWithStringId(R.string.date_create_report).performClick()
+            onNodeWithStringId(R.string.ok).performClick()
+        }
+    }
+
+    private fun currentDate(): String {
+        val date = Date().time
+        val format = SimpleDateFormat("\"dd\" MM yyyy")
+        return format.format(date)
     }
 }
